a384d52633a49d57bb37c3a16bbd258bcce37826
Adebayo Abubakri <adebayoabubakriolakayode12345@gmail.com>
2026-08-21 08:54:34 -0700

    fix Vite PostCSS production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
@@ -8,5 +9,12 @@ export default defineConfig({
     nitro({
       preset: "vercel",
     }),
+    tailwindcss(),
   ],
+
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
 });
