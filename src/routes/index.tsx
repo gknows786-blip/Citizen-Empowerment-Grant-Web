bf260e9293e02725b2a6fd152df06babb790493b
Adebayo Abubakri <adebayoabubakriolakayode12345@gmail.com>
2026-08-31 08:18:33 -0700

    Fix info boxes responsive layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -322,7 +322,7 @@ function Testimonials() {
     <div className="bg-slate-100 py-9 px-4 sm:px-6 border-y border-slate-200">
       <div className="max-w-7xl mx-auto">
         <div className="text-center mb-12">
-          <h2 className="text-3xl sm:text-4xl font-bold font-serif text-blue-950">
+          <h2 className="text-1.5xl sm:text-4xl font-bold font-serif text-blue-950">
             Beneficiary Testimonials
           </h2>
           <p className="text-slate-600 mt-2 text-sm sm:text-base">
