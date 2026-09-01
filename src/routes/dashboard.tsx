e5987a27e50ad6556fa740e19267e7f59494d61d
Adebayo Abubakri <adebayoabubakriolakayode12345@gmail.com>
2026-09-01 07:06:58 -0700

    asjgsbjf

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -388,7 +388,7 @@ function Dashboard() {
                         </p>
                       </div>
                       <div className="border-t border-slate-100 pt-2 mb-4 text-xs text-slate-600">
-                        Markdown:{" "}
+                        Imbursement:{" "}
                         <span className="font-bold text-slate-900">${pkg.feeRequired}</span>
                       </div>
                     </div>
